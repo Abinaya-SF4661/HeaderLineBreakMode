c09fc5500e3fb4eacf2695a635137212ac42dde1
Abinaya-SF4661 <abinaya.srinivasan@syncfusion.com>
2024-11-20 14:13:54 +0530

    Sample added

---
+++ MauiApp1/Model.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string _orderId;
+        private string _customerId;
+        private string _customer;
+        private string _city;
+        private string _country;
+
+        public event PropertyChangedEventHandler? RecordePropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string OrderId
+        {
+            get { return _orderId; }
+            set
+            {
+                _orderId = value;
+                RecordePropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(OrderId)));
+            }
+        }
+        public string CustomerId { get { return _customerId; } set { _customerId = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomerId))); } }
+        public string Customer { get { return _customer; } set { _customer = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Customer))); } }
+        public string City { get { return _city; } set { _city = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(City))); } }
+        public string Country { get { return _country; } set { _country = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Country))); } }
+        
+        public Model(string orderId, string customerID, string customer, string city, string country)
+        {
+            OrderId = orderId;
+            CustomerId = customerID;
+            Customer = customer;
+            City = city;
+            Country = country;
+        }
+    }
+}
---
+++ MauiApp1/ViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class ViewModel : INotifyCollectionChanged
+    {
+
+        private ObservableCollection<Model> _orders;
+        public ObservableCollection<Model> Orders { get { return _orders; } set { _orders = value; CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add)); } }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ViewModel()
+        {
+            Orders = new ObservableCollection<Model>();
+            this.GenerateOrders();
+        }
+
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        public void GenerateOrders()
+        {
+
+            var image = ImageSource.FromResource("DataGridSample.Resources.Images.syncfusion_logo1.png", typeof(Model).GetTypeInfo().Assembly);
+
+            Orders.Add(new Model("1001", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("1002", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("1003", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("1004", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("1005", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("1006", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            Orders.Add(new Model("10011", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10012", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("10013", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("10014", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("10015", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10016", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            Orders.Add(new Model("10021", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10022", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("10023", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("10024", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("10025", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10026", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            Orders.Add(new Model("1001", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("1002", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("1003", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("1004", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("1005", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("1006", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            Orders.Add(new Model("10011", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10012", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("10013", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("10014", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("10015", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10016", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+            Orders.Add(new Model("10021", "Thomas Hardy", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10022", "Ana Trujillo", "Mexico", "ANATR", "Mexico D.F."));
+            Orders.Add(new Model("10023", "Ant Fuller", "Mexico", "ANTON", "Mexico D.F."));
+            Orders.Add(new Model("10024", "Thomas Hardy", "UK", "AROUT", "London"));
+            Orders.Add(new Model("10025", "Tim Adams", "Sweden", "BERGS", "London"));
+            Orders.Add(new Model("10026", "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
+
+        }
+    }
+}
